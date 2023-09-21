f1c8b230e6d2b39a523c23452d599596c43e28bb
Csolly89 <141281525+Csolly89@users.noreply.github.com>
2023-09-20 20:02:35 -0400

    finished

--- tacos.js
+++ tacos.js
@@ -4,10 +4,20 @@ let api_base_url = "https://60d23844858b410017b2d60b.mockapi.io/tacos"
 //using fetch go print the tacos by id and by all
 async function getAllTacos(){
     //not returning data only console.log it
+    let answer = await fetch(api_base_url)
+    console.log(answer)
+
+    let products = await answer.json()
+    console.log("products", products)
 }
 
-async function getTacosById(){
+async function getTacosById(tacoId){
 //not returning data only console.log it
+let responses = await fetch(api_base_url +"/"+ tacoId)
+console.log(responses)
+
+let products = await responses.json()
+console.log("products", products)
 }
 
 //use get all, to print all tacos
